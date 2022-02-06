62d86e928a23245991d5e2bdba5c6d14fbe1a917
Caleb Mennen <git@calebmennen.com>
2022-02-06 11:48:06 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>I'm busy enough that I've stopped maintaining this website.</p>
+        <p>If you want to contact me, email me at hello -at- calebmennen.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
